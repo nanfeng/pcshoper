19d3c9d9164a296df699a4b049aca9e6aaccb12b
qianxin <qianxin@staff.sina.com.cn>
2018-09-20 17:56:53 +0800

    replace <br> to ,

--- watsons/All.js
+++ watsons/All.js
@@ -21,7 +21,7 @@
                 p_html += 'data.name = jQuery("h1[itemprop=' + '\'name' + '\']").text().trim();';
                 p_html += 'data.ingredient = "";';
                 p_html += 'data.Directions = "";';
-                p_html += 'jQuery(".drop-box").children().each(function () {var str = jQuery(this).text().trim();if (str.startsWith("Directions")) {    data.Directions = jQuery(this).next().text();}if (str.startsWith("Ingredients")) {  data.ingredient = jQuery(this).next().text();}});';
+                p_html += 'jQuery(".drop-box").children().each(function () {var str = jQuery(this).text().trim();if (str.startsWith("Directions")) {    data.Directions = jQuery(this).next().text();}if (str.startsWith("Ingredients")) {  data.ingredient = jQuery(this).next()..html().replace(/\<br\>/g, ",");}});';
                 p_html += ' jQuery("#ids").val(JSON.stringify(data));';
                 p_html += ' jQuery("#ids").select();';
                 p_html += ' document.execCommand("copy");';
@@ -61,7 +61,7 @@
                 p_html += 'if(!data.name.toLowerCase().startsWith(data.brand.toLowerCase())){data.name = data.brand + " " + data.name};';
                 p_html += 'data.ingredient = "";';
                 p_html += 'data.Directions = "";';
-                p_html += 'jQuery("#tabItem1").children().each(function () {var str = jQuery(this).text().trim();if (str.startsWith("Directions")) {    data.Directions = jQuery(this).next().text();};if (str.startsWith("Ingredients")) {  data.ingredient = jQuery(this).next().text();}});';
+                p_html += 'jQuery("#tabItem1").children().each(function () {var str = jQuery(this).text().trim();if (str.startsWith("Directions")) {    data.Directions = jQuery(this).next().text();};if (str.startsWith("Ingredients")) {  data.ingredient = jQuery(this).next().html().replace(/\<br\>/g, ",");}});';
                 p_html += ' jQuery("#ids").val(JSON.stringify(data));';
                 p_html += ' jQuery("#ids").select();';
                 p_html += ' document.execCommand("copy");';
